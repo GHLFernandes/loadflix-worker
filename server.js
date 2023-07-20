ad9ef5cef5088a4d1e070acb1f6d88f47556b6fc
Gabriel Fernandes <fernandes.gh@outlook.com>
2023-07-19 21:44:12 -0300

    feat: videoId

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const childProcess = require('child_process');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3002; // Porta da sua aplicação principal (Next.js)
+const PORT = process.env.PORT || 3002; // Adicione a opção process.env.PORT para obter a porta dinamicamente do Heroku
 
 // Rota para reproduzir o vídeo
 app.get('/executar-video', (req, res) => {
@@ -15,32 +15,45 @@ app.get('/executar-video', (req, res) => {
   res.status(200).json({ message: 'Vídeo executado com sucesso.' });
 });
 
-// Função para agendar a execução do vídeo
-const agendarExecucaoDoVideo = () => {
-  // Agendando a execução da mensagem 'teste' para ocorrer todos os dias às 12:00 (horário do servidor)
-  cron.schedule('* * * * *', async () => {
-    try {
-      // Executar o worker externo usando um processo filho com a porta 4000
-      childProcess.exec('node worker.js', { env: { PORT: 4000 } }, (error, stdout, stderr) => {
-        if (error) {
-          console.error('Erro ao executar o worker:', error);
-        } else {
-          console.log('Worker executado com sucesso!');
-          console.log(stdout);
-        }
-      });
-      
-      // Enviar uma requisição HTTP para a URL da sua aplicação Next.js para exibir a mensagem
-      await axios.get('http://localhost:3000/live/liveTest');
-      console.log('Mensagem exibida na página!');
-    } catch (error) {
-      console.error('Erro ao agendar a execução do vídeo:', error.message);
-    }
-  });
+// Função para executar o worker
+const executarWorker = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/api/videosPlaylist', {
+      headers: {
+        Cookie: 'next-auth.session-token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..JDm2JkCoJ4gWf9p1._Jj9TeS2GZv-nhzTPrfAboibAeXZfNLe0zhWACEsZD9LBFKgPUX3BA3yGVxjCJVYAnbjlgVwDc9VzbGI9tYyqV6nJb_AQMjFkrbEguYPuV-9TlGEnaeFV5VFYIH_OIh3QxBevO-U1i8QkpOLpxyZSt9ZloIT7w3rmURcGK-tzE0kpj1mS3WCx4Z_a9KniadcwkRIVzOeaAJpSYl-yl3vn2gxzxrJtQnU2A4a_sS7D_ARwHlQdOncBv6Ryygd20nWB3tA0I9YTqgwBua4uh8RJ6de8Y_Itp8OxEvxqHFq8NvQkb3foSazxfVKq4FT8xHkyV6PvK8kzwpWTxWaW1akqyPlYJtHPK2t3QBwBP2OWmoUolZXdeBBPm3t86Q.55w5NK0vn4UfFD98GE6iRA; Path=/; Expires=Sat, 19 Aug 2023 00:16:37 GMT; HttpOnly; SameSite=Lax'
+      }
+    });
+
+    let playlist = [];
+
+    response.data.forEach((item) => {
+      if (!playlist.includes(item.videoId)) {
+        playlist.push(item.videoId);
+      }
+    });
+
+    // Enviar uma requisição HTTP para a URL da sua aplicação Next.js para exibir a mensagem
+    await axios.get('http://localhost:3000/api/job', {
+      params: { mensagem: playlist[0] }
+    });
+    console.log('Mensagem enviada com sucesso!');
+  } catch (error) {
+    console.error('Erro ao enviar a mensagem:', error.message);
+  }
 };
 
-// Inicie o servidor e agende a execução do vídeo
+// Rota para iniciar o worker manualmente
+app.get('/start-worker', (req, res) => {
+  executarWorker();
+  res.status(200).json({ message: 'Worker iniciado manualmente.' });
+});
+
+// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-  agendarExecucaoDoVideo();
+  
+  // Agendar a execução do vídeo usando cron
+  cron.schedule('*/20 * * * *', () => {
+    executarWorker();
+  });
 });
--- worker.js
+++ worker.js
@@ -4,9 +4,36 @@ const axios = require('axios');
 
 cron.schedule('*/20 * * * * *', async () => {
   try {
+    const response = await axios.get('http://localhost:3000/api/videosPlaylist', {
+      headers: {
+        Cookie: 'next-auth.session-token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..JDm2JkCoJ4gWf9p1._Jj9TeS2GZv-nhzTPrfAboibAeXZfNLe0zhWACEsZD9LBFKgPUX3BA3yGVxjCJVYAnbjlgVwDc9VzbGI9tYyqV6nJb_AQMjFkrbEguYPuV-9TlGEnaeFV5VFYIH_OIh3QxBevO-U1i8QkpOLpxyZSt9ZloIT7w3rmURcGK-tzE0kpj1mS3WCx4Z_a9KniadcwkRIVzOeaAJpSYl-yl3vn2gxzxrJtQnU2A4a_sS7D_ARwHlQdOncBv6Ryygd20nWB3tA0I9YTqgwBua4uh8RJ6de8Y_Itp8OxEvxqHFq8NvQkb3foSazxfVKq4FT8xHkyV6PvK8kzwpWTxWaW1akqyPlYJtHPK2t3QBwBP2OWmoUolZXdeBBPm3t86Q.55w5NK0vn4UfFD98GE6iRA; Path=/; Expires=Sat, 19 Aug 2023 00:16:37 GMT; HttpOnly; SameSite=Lax'
+      }
+    });
+
+    // if(response){
+    //   console.log('RESULT', response.data);
+    // }
+
+    let playlist = [];
+    //let videosPlaylist = [];
+
+    response.data.forEach((item) => {
+      if(playlist.length > 0){
+        if(playlist.indexOf(item.playlistId) === -1){
+          playlist.push(item.videoId);
+        }
+      }else{
+        playlist.push(item.videoId);
+      }
+
+    
+    })
+
+    //console.log(playlist);
+
     // Enviar uma requisição HTTP para a URL da sua aplicação Next.js para exibir a mensagem
     await axios.get('http://localhost:3000/api/job', {
-      params: { mensagem: 'Mensagem de teste enviada pelo worker!' }
+      params: { mensagem: playlist[0] }
     });
     console.log('Mensagem enviada com sucesso!');
   } catch (error) {
